90ca5f6385442ff0247a54d0531e644a4f48e36c
Daniel <danidp25.08@gmail.com>
2024-02-28 19:06:07 +0100

    Add -> Roles y permisos en seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,6 +8,8 @@ use App\Models\Pub_type;
 use App\Models\Publication;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -33,7 +35,47 @@ class DatabaseSeeder extends Seeder
         $user->phone = '1234';
         $user->save();
 
+        $userAdmin = new User();
+        $userAdmin->name = 'Prueba Admin';
+        $userAdmin->email = 'admin@gmail.com';
+        $userAdmin->password = encrypt('1234');
+        $userAdmin->phone = '1234';
+        $userAdmin->save();
+
 
         $publicacion->commerces()->attach($user->id);
+
+
+        //Seeder Usuarios y permisos 
+        
+        $rol_admin = Role::create(['name' => 'admin']);
+        $rol_commerce = Role::create(['name' => 'commerce']);
+        $rol_customer = Role::create(['name' => 'customer']);
+
+        $perm_see_publication = Permission::create(['name' => 'see publication']);
+        $perm_edit_publication = Permission::create(['name' => 'edit publication']);
+        $perm_see_commerce = Permission::create(['name' => 'see commerce']);
+        $perm_edit_commerce = Permission::create(['name' => 'edit commerce']);
+        $perm_see_customer = Permission::create(['name' => 'see customer']);
+        $perm_edit_customer = Permission::create(['name' => 'edit customer']);
+        $perm_admin = Permission::create(['name' => 'admin']);
+
+        $rol_customer->givePermissionTo($perm_see_publication);
+        $rol_customer->givePermissionTo($perm_see_commerce);
+        $rol_customer->givePermissionTo($perm_edit_customer);
+
+        $rol_commerce->givePermissionTo($perm_see_commerce);
+        $rol_commerce->givePermissionTo($perm_see_customer);
+        $rol_commerce->givePermissionTo($perm_edit_commerce);
+        $rol_commerce->givePermissionTo($perm_edit_publication);
+        $rol_commerce->givePermissionTo($perm_see_publication);
+
+        $rol_admin->givePermissionTo($perm_admin);
+        $rol_admin->givePermissionTo($perm_see_commerce);
+        $rol_admin->givePermissionTo($perm_edit_commerce);
+        $rol_admin->givePermissionTo($perm_see_customer);
+        $rol_admin->givePermissionTo($perm_edit_customer);
+        $rol_admin->givePermissionTo($perm_see_publication);
+        $rol_admin->givePermissionTo($perm_edit_publication);
     }
 }
